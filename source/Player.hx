077d9bde8940c2841f56eee9354b4a739ef5608c
Noxalus <noxalus@gmail.com>
2017-07-05 22:12:22 +0200

    Change the level design and make a simple platformer gameplay.

--- source/Player.hx
+++ source/Player.hx
@@ -3,21 +3,19 @@ package;
 import flixel.FlxSprite;
 import flixel.FlxG;
 import flixel.FlxObject;
-import flixel.math.FlxPoint;
 
 class Player extends FlxSprite
 {
-    public var speed:Float = 300;
-
     public function new(?X:Float=0, ?Y:Float=0)
     {
         super(X, Y);
-        loadGraphic(AssetPaths.tinybox__png, false, 16, 16);
+        loadGraphic(AssetPaths.tinybox__png, false);
         setFacingFlip(FlxObject.LEFT, false, false);
         setFacingFlip(FlxObject.RIGHT, true, false);
-        drag.x = drag.y = 1600;
-        setSize(14, 8);
-        offset.set(4, 2);
+
+		maxVelocity.set(80, 250);
+		acceleration.y = 400;
+		drag.x = maxVelocity.x;
     }
 
     override public function update(elapsed:Float):Void
@@ -28,46 +26,21 @@ class Player extends FlxSprite
 
     private function movement():Void
     {
-        var _up:Bool = false;
-        var _down:Bool = false;
-        var _left:Bool = false;
-        var _right:Bool = false;
-
-        _up = FlxG.keys.anyPressed([UP, Z]);
-        _down = FlxG.keys.anyPressed([DOWN, S]);
-        _left = FlxG.keys.anyPressed([LEFT, Q]);
-        _right = FlxG.keys.anyPressed([RIGHT, D]);
-
-        if (!_up && !_down && !_left && !_right) { return; }
-
-        var mA:Float = 0;
-        if (_up)
-        {
-            mA = -90;
-            if (_left) { mA -= 45; }
-            else if (_right) { mA += 45; }
-            facing = FlxObject.UP;
-        }
-        else if (_down)
-        {
-            mA = 90;
-            if (_left) { mA += 45; }
-            else if (_right) { mA -= 45; }
-            facing = FlxObject.DOWN;
-        }
-        else if (_left)
-        {
-            mA = 180;
-            facing = FlxObject.LEFT;
-        }
-        else if (_right)
-        {
-            facing = FlxObject.RIGHT;
-            mA = 0;
-        }
-
-
-        velocity.set(speed, 0);
-        velocity.rotate(FlxPoint.weak(0, 0), mA);
+        acceleration.x = 0;
+		
+		if (FlxG.keys.anyPressed([LEFT, A]))
+		{
+			acceleration.x = -maxVelocity.x * 4;
+		}
+		
+		if (FlxG.keys.anyPressed([RIGHT, D]))
+		{
+			acceleration.x = maxVelocity.x * 4;
+		}
+		
+		if (FlxG.keys.anyJustPressed([SPACE, UP, W]) && isTouching(FlxObject.FLOOR))
+		{
+			velocity.y = -maxVelocity.y / 1.25;
+		}
      }
  }
