358af28bee188ffcfbeb30b7da67a5c4263d3bc6
Noxalus <noxalus@gmail.com>
2017-07-06 21:29:07 +0200

    Enemy spawners have a random timer to spawn enemies.

--- source/EnemySpawner.hx
+++ source/EnemySpawner.hx
@@ -1,22 +1,30 @@
 package;
 
-import flixel.FlxState;
+import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.util.FlxTimer;
 
 class EnemySpawner extends FlxSprite
 {
     private var _playState:PlayState;
+    private var _spawnTimer:FlxTimer;
 
     public function new(PlayState:PlayState, ?X:Float=0, ?Y:Float=0)
     {
         super(X, Y);
         _playState = PlayState;
 
-        _playState.addEnemy(this.x, this.y);
+        _spawnTimer = new FlxTimer().start(FlxG.random.int(1, 5), spawnEnemy, 3);
     }
 
     override public function update(elapsed:Float):Void
     {
         super.update(elapsed);
+    }
+
+    private function spawnEnemy(Timer:FlxTimer):Void
+    {
+        Timer.reset(FlxG.random.int(1, 5));
+        _playState.addEnemy(this.x, this.y);
     }
 }
